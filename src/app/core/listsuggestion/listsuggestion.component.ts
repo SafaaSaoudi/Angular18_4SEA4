7744d068c2544c4749281dadce60a45158632421
SafaaSaoudi <safa.saoudi@esprit.tn>
2025-11-28 11:26:42 +0100

    s5p1

--- src/app/core/listsuggestion/listsuggestion.component.ts
+++ src/app/core/listsuggestion/listsuggestion.component.ts
@@ -8,17 +8,57 @@ import { Suggestion } from '../../models/suggestion';
 })
 export class ListsuggestionComponent {
 
-  sugg: Suggestion = {
+ Suggestions: Suggestion[] = [ 
+  { 
     id: 1, 
     title: 'Organiser une journée team building', 
-    description: 'Suggestion pour organiser une journée de team building pour renforcer les liens entre les membres de l\'équipe.', 
+    description: 'Suggestion pour organiser une journée de team building pour renforcer les liens entre les membres de l\'équipe.',
     category: 'Événements', 
     date: new Date('2025-01-20'), 
+    status: 'acceptee',
+    nbrLikes: 10
+  }, 
+  { 
+    id: 2, 
+    title: 'Améliorer le système de réservation', 
+    description: 'Proposition pour améliorer la gestion des réservations en ligne avec un système de confirmation automatique.', 
+    category: 'Technologie', 
+    date: new Date('2025-01-15'), 
+    status: 'refusee',
+    nbrLikes: 5 
+  }, 
+  { 
+    id: 3, 
+    title: 'Créer un système de récompenses', 
+    description: 'Mise en place d\'un programme de récompenses pour motiver les employés et reconnaître leurs efforts.', 
+    category: 'Ressources Humaines', 
+    date: new Date('2025-01-25'), 
+    status: 'refusee',
+    nbrLikes: 8 
+  }, 
+  { 
+    id: 4, 
+    title: 'Moderniser l\'interface utilisateur', 
+    description: 'Refonte complète de l\'interface utilisateur pour une meilleure expérience utilisateur.', 
+    category: 'Technologie', 
+    date: new Date('2025-01-30'), 
+    status: 'en_attente' ,
+    nbrLikes: 12
+  }, 
+  { 
+    id: 5, 
+    title: 'Formation à la sécurité informatique', 
+    description: 'Organisation d\'une formation sur les bonnes pratiques de sécurité informatique pour tous les employés.', 
+    category: 'Formation', 
+    date: new Date('2025-02-05'), 
     status: 'acceptee' ,
-    nbrLikes: 1
-  }
+    nbrLikes: 7
+  }]; 
 
   likeSuggestion(id: number) {
-    this.sugg.nbrLikes += 1;
+    const suggestion = this.Suggestions.find(s => s.id === id);
+    if (suggestion) {
+      suggestion.nbrLikes += 1;
+    }
   }
 }
