83b307983dbec68ea6a721efb56a0893f396897a
SafaaSaoudi <safa.saoudi@esprit.tn>
2025-11-28 12:18:08 +0100

    s5p2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListsuggestionComponent } from './core/listsuggestion/listsuggestion.component';
 import { HomeComponent } from './core/home/home.component';
 import { NotfoundComponent } from './core/notfound/notfound.component';
+import { DetailssuggestionComponent } from './core/detailssuggestion/detailssuggestion.component';
 
 const routes: Routes = [
   
   {path:'', redirectTo:'home', pathMatch:'full'},
-  {path:'List', component:ListsuggestionComponent},
+  {path:'List', component:ListsuggestionComponent,
+    children:[{path:"suggestionDetails/:X", component:DetailssuggestionComponent}]
+  },
   {path: 'home', component:HomeComponent},
+  
   {path:'**', component:NotfoundComponent}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { FooterComponent } from './core/footer/footer.component';
 import { ListsuggestionComponent } from './core/listsuggestion/listsuggestion.component';
 import { HomeComponent } from './core/home/home.component';
 import { NotfoundComponent } from './core/notfound/notfound.component';
+import { DetailssuggestionComponent } from './core/detailssuggestion/detailssuggestion.component';
 
 @NgModule({
   declarations: [ //liste des composants
-    AppComponent, HeaderComponent, FooterComponent, ListsuggestionComponent, HomeComponent, NotfoundComponent
+    AppComponent, HeaderComponent, FooterComponent, ListsuggestionComponent, HomeComponent, NotfoundComponent, DetailssuggestionComponent
   ],
   imports: [
     BrowserModule,
